dbf49e741ddd9cb6251310e1cf94170fcf5f9a5d
Mike Palarz <mpalarz1@gmail.com>
2018-11-15 20:49:22 -0600

    feat: Completed chapter 16

--- src/com/palarz/mike/nyethack/Game.kt
+++ src/com/palarz/mike/nyethack/Game.kt
@@ -1,5 +1,7 @@
 package com.palarz.mike.nyethack
 
+import kotlin.system.exitProcess
+
 fun main(args: Array<String>) {
 
     Game.play()
@@ -53,12 +55,37 @@ object Game{
                 "Invalid direction: $directionInput"
             }
 
+    private fun fight() = currentRoom.monster?.let {
+        while (player.healthPoints > 0 && it.healthPoints > 0) {
+            slay(it)
+            Thread.sleep(1000)
+        }
+
+        "Combat complete."
+    } ?: "There's nothing here to fight."
+
+    private fun slay(monster: Monster) {
+        println("${monster.name} did ${monster.attack(player)} damage!")
+        println("${player.name} did ${player.attack(monster)} damage!")
+
+        if (player.healthPoints <= 0) {
+            println(">>> You have been defeated! Thanks for playing. <<<")
+            exitProcess(0)
+        }
+
+        if (monster.healthPoints <= 0) {
+            println(">>> ${monster.name} has been defeated! <<<")
+            currentRoom.monster = null
+        }
+    }
+
     private class GameInput(arg: String?){
         private val input = arg ?: ""
         val command = input.split(" ")[0]
         val argument = input.split(" ").getOrElse(1, {""})
 
         fun processCommand() = when(command.toLowerCase()){
+            "fight" -> fight()
             "move" -> move(argument)
             else -> commandNotFound()
         }
--- src/com/palarz/mike/nyethack/Player.kt
+++ src/com/palarz/mike/nyethack/Player.kt
@@ -2,7 +2,23 @@ package com.palarz.mike.nyethack
 
 import java.io.File
 
-class Player(_name: String, var healthPoints: Int = 100, val isBlessed: Boolean, private val isImmortal: Boolean){
+class Player(_name: String,
+             override var healthPoints: Int = 100,
+             val isBlessed: Boolean,
+             private val isImmortal: Boolean) : Fightable{
+
+    override val diceCount = 3
+    override val diceSides = 6
+
+    override fun attack(opponent: Fightable): Int {
+        val damageDealt = if (isBlessed){
+            damageRoll * 2
+        } else{
+            damageRoll
+        }
+        opponent.healthPoints -= damageDealt
+        return damageDealt
+    }
 
     var name = _name
     get() = "${field.capitalize()} of $hometown"
